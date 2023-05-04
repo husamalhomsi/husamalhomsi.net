33a7daac70c19def1714ded750dad80a9235c188
Hussam al-Homsi <sawuare@gmail.com>
2023-05-04 01:23:24 -0400

    58

--- sh/hxy.sh
+++ sh/hxy.sh
@@ -6,8 +6,8 @@ hxy -z -l 509
 hxy -z -l 512 -s
 hxy -z -l 509 -s
 
-hxy -z -l 512 -c
-hxy -z -l 509 -c
+hxy -z -l 512 -H
+hxy -z -l 509 -H
 
-hxy -z -l 512 -r
-hxy -z -l 509 -r
+hxy -z -l 512 -V
+hxy -z -l 509 -V
