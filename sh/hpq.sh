980f307fdffd9b3eaa0866cf1aeaf3bf3cd60ca7
Hussam al-Homsi <sawuare@gmail.com>
2023-05-12 13:33:32 -0400

    61

--- sh/hpq.sh
+++ sh/hpq.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-hpq-png -z -p 0 -q 448
-hpq-png -z -p 1 -q 448
-hpq-png -z -p 2 -q 448
-hpq-png -z -p 3 -q 448
-hpq-png -z -p 4 -q 448
+hpq-png -z -p 0 -q 192
+hpq-png -z -p 1 -q 192
+hpq-png -z -p 2 -q 192
+hpq-png -z -p 3 -q 192
+hpq-png -z -p 4 -q 192
 
 hpq-png -z -p 5 -q 19
 hpq-png -z -p 6 -q 19
@@ -12,22 +12,21 @@ hpq-png -z -p 7 -q 19
 hpq-png -z -p 8 -q 19
 hpq-png -z -p 9 -q 19
 
-hpq-png -z -p 10 -q 16384
-hpq-png -z -p 11 -q 16384
-hpq-png -z -p 12 -q 16384
-hpq-png -z -p 13 -q 16384
-hpq-png -z -p 14 -q 16384
+hpq-png -z -p 10 -q 4096
+hpq-png -z -p 11 -q 4096
+hpq-png -z -p 12 -q 4096
+hpq-png -z -p 13 -q 4096
+hpq-png -z -p 14 -q 1048576
 
-hpq-png -z -p 19 -q 19
+hpq-png -z -p 21 -q 223
+hpq-png -z -p 22 -q 2040
 
-hpq-png -z -p 21 -q 447
-hpq-png -z -p 22 -q 16352
-
-hpq-png -z -p 25 -q 264
+hpq-png -z -p 25 -q 136
 hpq-png -z -p 26 -q 15
 hpq-png -z -p 27 -q 15
+hpq-png -z -p 29 -q 4097
 
 hpq-png -z -p 35 -q 20
-hpq-png -z -p 36 -q 63
-hpq-png -z -p 37 -q 63
-hpq-png -z -p 39 -q 65537
+hpq-png -z -p 36 -q 15
+hpq-png -z -p 37 -q 15
+hpq-png -z -p 39 -q 1048577
--- sh/hxy.sh
+++ sh/hxy.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
-hxy -z -l 512
-hxy -z -l 509
+hxy -z -l 256
+hxy -z -l 251
 
-hxy -z -l 512 -s
-hxy -z -l 509 -s
+hxy -z -l 256 -s
+hxy -z -l 251 -s
 
-hxy -z -l 512 -H
-hxy -z -l 509 -H
+hxy -z -l 256 -H
+hxy -z -l 251 -H
 
-hxy -z -l 512 -V
-hxy -z -l 509 -V
+hxy -z -l 256 -V
+hxy -z -l 251 -V
