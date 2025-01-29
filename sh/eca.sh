5cbcf9f3f670ad0deb0b76e59e7302c35d536c08
Husam Alhomsi <sawuare@gmail.com>
2025-01-29 13:17:42 -0500

    183

--- sh/eca.sh
+++ sh/eca.sh
@@ -4,10 +4,10 @@ set -e
 
 cd ../img/eca/
 
-eca-png -c 512 -g 256 -r 18
-eca-png -c 512 -g 256 -r 30
+eca-png -r 18 -c 512 -g 256
+eca-png -r 30 -c 512 -g 256
 
 cd ../../audio/eca/
 
-eca-wav -c 512 -g 256 -r 18
-eca-wav -c 512 -g 256 -r 30
+eca-wav -r 18 -c 512 -g 256
+eca-wav -r 30 -c 512 -g 256
