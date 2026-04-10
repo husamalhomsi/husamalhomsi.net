7a0380b56947ce9cc1d9a4b32bcfe4cbcfa3277e
Husam Alhomsi <sawuare@gmail.com>
2026-04-10 02:35:10 -0400

    256

--- sh/maolp.sh
+++ sh/maolp.sh
@@ -2,15 +2,26 @@
 
 set -e
 
-xy-png  -z -w 2048 -h 1536
-pq-png  -z -w 2048 -h 1536 -p 14 -q 33554432 -x -512 -y -384
-eca-png -z -c 2048 -g 1536 -r 120 -s 1
+H=1024
+W=$((H*4/3))
+P=14
+Q=$((2**24))
+X=$((-W/4))
+Y=$((-H/4))
+R=120
+S=1
 
-mv *.pq.png p14q33554432w2048h1536x-512y-384.pq.png
+xy-png  -z -w $W -h $H
+pq-png  -z -w $W -h $H -p $P -q $Q -x $X -y $Y
+eca-png -z -c $W -g $H -r $R -s $S
 
-img2doc *.xy.png  w2048h1536.xy.pdf
-img2doc *.pq.png  p14q33554432w2048h1536x-512y-384.pq.pdf
-img2doc *.eca.png r120s1c2048g1536.eca.pdf
+mv *.pq.png p${P}q${Q}w${W}h${H}x${X}y${Y}.pq.png
+
+img2doc *.xy.png  w${W}h${H}.xy.pdf
+img2doc *.pq.png  p${P}q${Q}w${W}h${H}x${X}y${Y}.pq.pdf
+img2doc *.eca.png r${R}s${S}c${W}g${H}.eca.pdf
+
+rm -f ../img/maolp/* ../doc/maolp/*
 
 mv *.png ../img/maolp/
-mv *.pdf ../pdf/maolp/
+mv *.pdf ../doc/maolp/
